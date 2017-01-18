b46dd3b07270952fb6ad720ac64dde4d51082f20
Mauricio Cabrera <mauseb91@gmail.com>
2017-01-18 12:47:26 -0500

    Jquery

--- ProyectoSCS/tasks/pipeline.js
+++ ProyectoSCS/tasks/pipeline.js
@@ -31,9 +31,13 @@ var jsFilesToInject = [
   // Dependencies like jQuery, or Angular are brought in here
   'js/dependencies/**/*.js',
 
+    
+    'js/jquery.js',
+    
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
   'js/**/*.js'
+    
 ];
 
 
