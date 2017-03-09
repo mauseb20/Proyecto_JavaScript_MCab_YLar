552cb9d615aa4507ad6fc59328e3db3b617f6baa
Mauricio Cabrera <mauseb91@gmail.com>
2017-03-09 16:24:53 -0500

    Fin programa

--- ProyectoSCS/api/controllers/ProgramaController.js
+++ ProyectoSCS/api/controllers/ProgramaController.js
@@ -98,7 +98,12 @@ module.exports = {
                     }
                 })
             }else{
-                return res.badRequest('No envia todos los parametros');
+                return res.view('error',{
+                    title: 'software',
+                    tituloError: 'error',
+                    error: 'Algunos campos obligatorio se encuentran vacios',
+                    url: '/crearSoftware'
+                })
             }
         }else{
             return res.badRequest('Metodo invalido');
@@ -116,7 +121,11 @@ module.exports = {
             }).exec(function(error,programaEliminado){
                 if (error) return res.serverError()
 
-                Programa.find().exec(function(error,swEncontrado){
+                Programa.find({
+                    tipoProg: {
+                        contains: 'APP'
+                    }
+                }).exec(function(error,swEncontrado){
                     if(error) return res.serverError()
 
                     return res.view('Software/Software',{
@@ -134,56 +143,96 @@ module.exports = {
 
         var parametros =req.allParams();
         if (req.method=='POST'){
-            if(parametros.nombrePrograma&&parametros.tipoProg&&parametros.capacidad&&parametros.numOrdenadores){
-                console.log(parametros);
-                Programa.findOne({
+            if(parametros.nombrePrograma && parametros.categoria && parametros.versionProg){
+                var swAEditar = {
                     nombrePrograma: parametros.nombrePrograma,
-                            tipoProg: tipoPrograma,
-                            servicio: parametros.servicio,
-                            categoria: parametros.categoria,
-                            versionProg: parametros.versionProg,
-                            anioProg: parametros.anioProg
-                }).exec(function(error,laboratorioEncontrado){
-                    if (error) return res.serverError();
-                    if (laboratorioEncontrado!=undefined){
-                        return res.view('error',{
-                            title: 'laboratorios',
-                            tituloError: 'error',
-                            error: 'El Laboratorio '+parametros.nombreLaboratorio+' ya existe',
-                            url: '/laboratorios'
-                        })
-                    }else{ 
-
-                        Programa.update({
-                            idLaboratorio: parametros.idLaboratorio
-                        },{
-                            nombreLaboratorio: parametros.nombreLaboratorio,
-                            numAula: parametros.numAula,
-                            capacidad: parametros.capacidad,
-                            descripcionUbicacion: parametros.descripcionUbicacion,
-                            numOrdenadores: parametros.numOrdenadores,
-                            proyectorEmpotrado: parametros.proyectorEmpotrado
-                        }).exec(function(error,laboratorioCreado){
-                            if (error) return res.serverError();
-                            console.log(laboratorioCreado);
-                            Laboratorio.find().exec(function(error,laboratoriosEncontrados){
-                                if(error) return res.serverError()
-                                //sails.log.info(laboratoriosEncontrados);
-                                return res.view('Laboratorios/Laboratorios',{
-                                    title: 'laboratorios',
-                                    tituloError: '',
-                                    laboratorio: laboratoriosEncontrados
+                    servicio: parametros.servicio,
+                    categoria: parametros.categoria,
+                    versionProg: parametros.versionProg,
+                    anioProg: parametros.anioProg
+                }
+                Programa.findOne({
+                    idPrograma: parametros.idPrograma
+                }).exec(function(error,programaEncontrado){
+                    if(parametros.nombrePrograma == programaEncontrado.nombrePrograma){
+                        delete swAEditar.nombrePrograma
+                    }
+                    if(parametros.versionProg == programaEncontrado.versionProg){
+                        delete swAEditar.versionProg
+                    }
+                    if(parametros.categoria == programaEncontrado.categoria){
+                        delete swAEditar.categoria
+                    }
+                    if(parametros.anioProg == programaEncontrado.anioProg){
+                        delete swAEditar.anioProg
+                    }
+                    if(parametros.servicio == programaEncontrado.servicio){
+                        delete swAEditar.servicio
+                    }
+                    if(swAEditar.nombrePrograma||swAEditar.servicio||swAEditar.versionProg||swAEditar.anioProg||swAEditar.categoria){
+                        Programa.findOne({
+                            nombrePrograma: swAEditar.nombrePrograma
+                        }).exec(function(error,softwareEncontrado){
+                            if(softwareEncontrado){
+                                if(softwareEncontrado.versionProg==swAEditar.versionProg){
+                                    return res.view('error',{
+                                        title: 'software',
+                                        tituloError: 'error',
+                                        error: 'El software '+parametros.nombrePrograma+' ya existe',
+                                        url: '/software'
+                                    })
+                                }else{
+                                    Programa.update({
+                                        idPrograma: parametros.idPrograma
+                                    },swAEditar).exec(function(error,programaEditado){
+                                        Programa.find({
+                                            tipoProg: {
+                                                contains: 'APP'
+                                            }
+                                        }).exec(function(error, softwareEncontrado){
+                                            if (error) return res.serverError();
+                                            return res.view('Software/Software', {
+                                                title: 'software',
+                                                tituloError: '',
+                                                software: softwareEncontrado
+                                            })
+                                        })
+                                    })
+                                }
+                            }else{
+                                Programa.update({
+                                    idPrograma: parametros.idPrograma
+                                },swAEditar).exec(function(error,programaEditado){
+                                    Programa.find({
+                                        tipoProg: {
+                                            contains: 'APP'
+                                        }
+                                    }).exec(function(error, softwareEncontrado){
+                                        if (error) return res.serverError();
+                                        return res.view('Software/Software', {
+                                            title: 'software',
+                                            tituloError: '',
+                                            software: softwareEncontrado
+                                        })
+                                    })
                                 })
-                            })
+                            }
+                        })
+                    }else{
+                        return res.view('error',{
+                            title: 'software',
+                            tituloError: 'error',
+                            error: 'El software '+parametros.nombrePrograma+' no ha sido editado',
+                            url: '/software'
                         })
                     }
                 })
             }else{
                 return res.view('error',{
-                    title: 'laboratorios',
+                    title: 'software',
                     tituloError: 'error',
-                    error: 'Algunos campos se encuentran vacios',
-                    url: '/laboratorios'
+                    error: 'Algunos campos obligatorio se encuentran vacios',
+                    url: '/software'
                 })
             }
         }else{
